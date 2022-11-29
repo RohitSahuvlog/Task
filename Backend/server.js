9fe578d23121c1814684fe0e372e578799bb2bc5
Rohit sahu <96680038+RohitSahuvlog@users.noreply.github.com>
2022-11-29 06:27:39 +0530

    done

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const cors = require("cors")
+const  {connection} = require("./config/db")
+const {userrouter} = require("./router/authuser.route")
+const {authentication} =require("./middlewares/authentication")
+const {managerouter} = require("./router/manageuser.route")
+const app = express()
+app.use(express.json())
+app.use(cors())
+PORT = 8000 ||  process.env.PORT
+
+app.get("/",()=>{
+    res.send("Home page")
+})
+app.use("/auth",userrouter)
+app.use(authentication)
+app.use("/manage",managerouter)
+
+
+app.listen(PORT,async(req,res)=>{
+
+
+    try{
+await connection
+console.log("connect to mongodb")
+
+    }
+    catch{
+
+        console.log(" error connect to mongodb") 
+    }
+
+    console.log(`server is start at ${PORT}`)
+})
